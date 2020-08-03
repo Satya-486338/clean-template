63a4096463a6a83f298d7298d6b61e1c2cc816bb
Satyendra Tiwari <satyendratiwari1991@gmail.com>
2020-08-03 17:58:34 +0530

    Update VehicleCollector.cpp

--- VehicleCollector.cpp
+++ VehicleCollector.cpp
@@ -1,1 +1,22 @@
-#include"VehicleCollectorTest.h"
+inventory telematics_to_inventory(telematics* obj_telematics)
+{
+  inventory inventory;
+  
+  inventory.vehicle_id = obj_telematics->vehicle_id;
+  
+  switch(obj_telematics->type)
+  {
+    case motor_temp:
+      inventory.motor_temp = obj_telematics->measurement;
+      break;
+    case battery_pc:
+      inventory.battery_pc = obj_telematics->measurement;
+      break;
+    case battery_temp:
+      inventory.battery_temp = obj_telematics->measurement;
+      break;
+    default:break;
+  }
+  
+  return inventory;
+}
