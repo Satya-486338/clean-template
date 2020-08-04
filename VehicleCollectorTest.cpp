f902585018f5abe510f03f1d93de98d18b37597e
Satyendra Tiwari <satyendratiwari1991@gmail.com>
2020-08-04 16:17:07 +0530

    Update VehicleCollectorTest.cpp

--- VehicleCollectorTest.cpp
+++ VehicleCollectorTest.cpp
@@ -2,7 +2,7 @@
 #include "VehicleCollectorTest.h"
 TEST(COLLECT, when_vehicle_reports_a_measurement_it_is_fetched_and_storted_with_vehicle_id){
    telematics obj_telematics = {231,motor_temp,30};
-   inventory obj_inventory   = telematics_to_inventory(&obj_telematics);
+   inventory obj_inventory   = telematics_to_inventory(obj_telematics);
  
     ASSERT_EQ(inventory.motor_temp,30);
 }
